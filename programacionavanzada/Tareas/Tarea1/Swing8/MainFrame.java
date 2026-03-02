fd51bd9a92461159805208fc6a154e76654868c2
AranzaJaquelineHernandezRosas <jaquehrnan26@gmail.com>
2026-03-01 19:27:15 -0600

    Add files via upload

---
+++ programacionavanzada/Tareas/Tarea1/Swing8/MainFrame.java
@@ -0,0 +1,34 @@
+package Swing8;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    
+    private TextPanel textPanel;
+    private Toolbar toolbar;
+    
+    public MainFrame() {
+        super("Hello World");
+        
+        setLayout(new BorderLayout());
+        
+        toolbar = new Toolbar();
+        textPanel = new TextPanel();
+        
+       
+        toolbar.setStringListener(new StringListener() {
+            @Override
+            public void textEmitted(String text) {
+               
+                textPanel.appendText(text);
+            }
+        });
+        
+        add(toolbar, BorderLayout.NORTH);
+        add(textPanel, BorderLayout.CENTER);
+        
+        setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
