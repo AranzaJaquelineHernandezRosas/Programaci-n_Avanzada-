ea911e5ed515d3cde0451f3596a62569772d9f4b
AranzaJaquelineHernandezRosas <jaquehrnan26@gmail.com>
2026-03-01 19:20:32 -0600

    Add files via upload

---
+++ programacionavanzada/Tareas/Tarea1/Swing18/App.java
@@ -0,0 +1,17 @@
+package Swing18;
+
+
+import javax.swing.SwingUtilities;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame();
+            }
+        });
+    }
+}
