7b0512bcc82bb5ae0191fc789b59ab770906be4d
AranzaJaquelineHernandezRosas <jaquehrnan26@gmail.com>
2026-03-01 19:16:27 -0600

    Add files via upload

---
+++ programacionavanzada/Tareas/Tarea1/Swing12/App.java
@@ -0,0 +1,16 @@
+package Swing12;
+import javax.swing.SwingUtilities;
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 SwingUtilities.invokeLater(new Runnable() {
+	            @Override
+	            public void run() {
+	                new MainFrame();
+	            }
+	        });
+
+	}
+
+}
